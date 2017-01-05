314f82f9f665ab49673a580f7a4115cf36bf5f65
Hyper <james_peter_wong@hotmail.com>
2017-01-06 12:33:41 +1300

    Formatting

--- src/core/hle/svc.cpp
+++ src/core/hle/svc.cpp
@@ -892,7 +892,7 @@ static ResultCode CreateMemoryBlock(Kernel::Handle* out_handle, u32 addr, u32 si
 
     // TODO(Subv): Processes with memory type APPLICATION are not allowed
     // to create memory blocks with addr = 0, any attempts to do so
-    // should return error 0xD92007EA. 
+    // should return error 0xD92007EA.
     if ((addr < Memory::PROCESS_IMAGE_VADDR || addr + size > Memory::SHARED_MEMORY_VADDR_END) &&
         addr != 0) {
         return ResultCode(ErrorDescription::InvalidAddress, ErrorModule::OS,
